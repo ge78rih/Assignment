425e38eeb38c6bbad5c9d3feb69016c4dece4fc9
ge78rih <ge78rih@tum.de>
2023-05-15 22:58:42 +0700

    Q2

--- Untitled.R
+++ Untitled.R
@@ -1,3 +1,5 @@
+library(tidyverse)
+
 ### Assignment 1 ###
 
 ## 2 Dice ##
@@ -6,12 +8,20 @@
 outcome <- c(1:6)
 outcome
 # a die consist of 6 numbers so possible outcome for a die can be 1 to 6
-N <- 10
-vec_1 <- sample(outcome, N, replace = T)
+N <- 6
+vec_1 <- sample(outcome, N, replace = F)
 vec_1
-vec_2 <- sample(outcome, N, replace = T)
+vec_2 <- sample(outcome, N, replace = F)
 vec_2
 # each vector represent a die throw at N time and same number can occur multiple times 
 
 
 ## Q2 
+sample_space <- expand_grid(die_1 = vec_1, die_2 = vec_2,)
+sample_space 
+
+
+
+
+
+
