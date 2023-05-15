256324f81e89571f2fe7dfa62e184bb05259572e
ge78rih <ge78rih@tum.de>
2023-05-15 22:21:32 +0700

    Q1

---
+++ Untitled.R
@@ -0,0 +1,17 @@
+### Assignment 1 ###
+
+## 2 Dice ##
+
+## Q1 
+outcome <- c(1:6)
+outcome
+# a die consist of 6 numbers so possible outcome for a die can be 1 to 6
+N <- 10
+vec_1 <- sample(outcome, N, replace = T)
+vec_1
+vec_2 <- sample(outcome, N, replace = T)
+vec_2
+# each vector represent a die throw at N time and same number can occur multiple times 
+
+
+## Q2 
